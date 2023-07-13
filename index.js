5db6057c2cc8286dc6ba5e85c2b66ba217e6c974
Veljko Govedarica <veljkovg@gmail.com>
2023-07-13 04:24:13 +0200

    promena

--- index.js
+++ index.js
@@ -1,62 +1,74 @@
 // Funkcija za učitavanje JSON fajla
 function loadJSON(url, callback) {
-    var xobj = new XMLHttpRequest();
-    xobj.overrideMimeType("application/json");
-    xobj.open('GET', url, true);
-    xobj.onreadystatechange = function () {
-      if (xobj.readyState == 4 && xobj.status == "200") {
-        callback(xobj.responseText);
+  var xobj = new XMLHttpRequest();
+  xobj.overrideMimeType("application/json");
+  xobj.open('GET', url, true);
+  xobj.onreadystatechange = function () {
+    if (xobj.readyState == 4 && xobj.status == "200") {
+      callback(xobj.responseText);
+    }
+  };
+  xobj.send(null);
+}
+
+// Funkcija za generisanje HTML koda kviz-pitanja
+function generateQuizHTML(quizData) {
+  var quizContainer = document.getElementById("quiz-container");
+  var html = "";
+
+  // Iteriraj kroz kviz-pitanja
+  for (var questionKey in quizData.quiz) {
+    if (quizData.quiz.hasOwnProperty(questionKey)) {
+      var question = quizData.quiz[questionKey];
+      html += '<div class="question">' + question.question + '</div>';
+
+      // Generiši opcije za odgovor
+      for (var i = 0; i < question.options.length; i++) {
+        var option = question.options[i];
+        var isChecked = localStorage.getItem(questionKey) === option ? 'checked' : '';
+        html += '<label>';
+        html += '<input type="radio" name="' + questionKey + '" value="' + option + '" ' + isChecked + '>' + option;
+        html += '</label>';
       }
-    };
-    xobj.send(null);
+
+      html += '<br><br>';
+    }
   }
-  
-  // Funkcija za generisanje HTML koda kviz-pitanja
-  function generateQuizHTML(quizData) {
-    var quizContainer = document.getElementById("quiz-container");
-    var html = "";
-  
-    // Iteriraj kroz kviz-pitanja
-    for (var questionKey in quizData.quiz) {
-      if (quizData.quiz.hasOwnProperty(questionKey)) {
-        var question = quizData.quiz[questionKey];
-        html += '<div class="question">' + question.question + '</div>';
-  
-        // Generiši opcije za odgovor
-        for (var i = 0; i < question.options.length; i++) {
-          var option = question.options[i];
-          var isChecked = localStorage.getItem(questionKey) === option ? 'checked' : '';
-          html += '<label>';
-          html += '<input type="radio" name="' + questionKey + '" value="' + option + '" ' + isChecked + '>' + option;
-          html += '</label>';
+
+  // Dodaj dugme za predaju kviza
+  html += '<button onclick="submitQuiz()">Submit</button>';
+
+  // Prikaz HTML koda u kontejneru
+  quizContainer.innerHTML = html;
+
+  // Postavi prethodno odabrane odgovore na osnovu lokalnog skladišta (local storage)
+  for (var questionKey in quizData.quiz) {
+    if (quizData.quiz.hasOwnProperty(questionKey)) {
+      var selectedOption = localStorage.getItem(questionKey);
+      if (selectedOption) {
+        var radioInput = document.querySelector('input[name="' + questionKey + '"][value="' + selectedOption + '"]');
+        if (radioInput) {
+          radioInput.checked = true;
         }
-  
-        html += '<br><br>';
       }
     }
-  
-    // Dodaj dugme za predaju kviza
-    html += '<button onclick="submitQuiz()">Submit</button>';
-  
-    // Prikaz HTML koda u kontejneru
-    quizContainer.innerHTML = html;
   }
-  
-  // Funkcija za čuvanje odgovora u local storage
-  function saveAnswer(questionKey, answer) {
-    localStorage.setItem(questionKey, answer);
-  }
-  
-  // Funkcija za predaju kviza (možete je prilagoditi svojim potrebama)
-  function submitQuiz() {
-    // Ovde možete dodati logiku za proveru tačnih odgovora i prikaz rezultata
-    // na osnovu odabranih opcija
-    console.log('Quiz submitted!');
-  }
-  
-  // Učitavanje JSON fajla i parsiranje
-  loadJSON('kviz.json', function (response) {
-    var quizData = JSON.parse(response);
-    generateQuizHTML(quizData);
-  });
-  
+}
+
+// Funkcija za čuvanje odgovora u local storage
+function saveAnswer(questionKey, answer) {
+  localStorage.setItem(questionKey, answer);
+}
+
+// Funkcija za predaju kviza (možete je prilagoditi svojim potrebama)
+function submitQuiz() {
+  // Ovde možete dodati logiku za proveru tačnih odgovora i prikaz rezultata
+  // na osnovu odabranih opcija
+  console.log('Quiz submitted!');
+}
+
+// Učitavanje JSON fajla i parsiranje
+loadJSON('kviz.json', function (response) {
+  var quizData = JSON.parse(response);
+  generateQuizHTML(quizData);
+});
