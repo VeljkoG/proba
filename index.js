0a5621ec55fd31d011fb985d837f5f02faf05c83
Veljko Govedarica <veljkovg@gmail.com>
2023-07-13 04:31:07 +0200

    e

--- index.js
+++ index.js
@@ -1,21 +1,3 @@
-// Funkcija za proveru podrške za navigator.userAgentData
-function isUserAgentDataSupported() {
-  return navigator.userAgentData && navigator.userAgentData.brands && Array.isArray(navigator.userAgentData.brands);
-}
-
-// Funkcija za učitavanje JSON fajla
-function loadJSON(url, callback) {
-  var xobj = new XMLHttpRequest();
-  xobj.overrideMimeType("application/json");
-  xobj.open('GET', url, true);
-  xobj.onreadystatechange = function () {
-    if (xobj.readyState == 4 && xobj.status == "200") {
-      callback(xobj.responseText);
-    }
-  };
-  xobj.send(null);
-}
-
 // Funkcija za generisanje HTML koda kviz-pitanja
 function generateQuizHTML(quizData) {
   var quizContainer = document.getElementById("quiz-container");
@@ -46,41 +28,26 @@ function generateQuizHTML(quizData) {
   // Prikaz HTML koda u kontejneru
   quizContainer.innerHTML = html;
 
+  // Postavi event listenere na radio input elemente kako bismo pratili promene odgovora
+  var radioInputs = quizContainer.querySelectorAll('input[type="radio"]');
+  radioInputs.forEach(function (radioInput) {
+    radioInput.addEventListener('change', function (event) {
+      var questionKey = event.target.name;
+      var selectedAnswer = event.target.value;
+      saveAnswer(questionKey, selectedAnswer);
+    });
+  });
+
   // Postavi prethodno odabrane odgovore na osnovu lokalnog skladišta (local storage)
   for (var questionKey in quizData.quiz) {
     if (quizData.quiz.hasOwnProperty(questionKey)) {
       var selectedOption = localStorage.getItem(questionKey);
       if (selectedOption) {
-        var radioInput = document.querySelector('input[name="' + questionKey + '"][value="' + selectedOption + '"]');
+        var radioInput = quizContainer.querySelector('input[name="' + questionKey + '"][value="' + selectedOption + '"]');
         if (radioInput) {
           radioInput.checked = true;
         }
       }
     }
   }
-}
-
-// Funkcija za čuvanje odgovora u local storage
-function saveAnswer(questionKey, answer) {
-  localStorage.setItem(questionKey, answer);
-}
-
-// Funkcija za predaju kviza (možete je prilagoditi svojim potrebama)
-function submitQuiz() {
-  // Ovde možete dodati logiku za proveru tačnih odgovora i prikaz rezultata
-  // na osnovu odabranih opcija
-  console.log('Quiz submitted!');
-}
-
-// Učitavanje JSON fajla i parsiranje
-loadJSON('kviz.json', function (response) {
-  var quizData = JSON.parse(response);
-  generateQuizHTML(quizData);
-});
-
-// Provera podrške za navigator.userAgentData
-if (isUserAgentDataSupported()) {
-  console.log('navigator.userAgentData je podržan.');
-} else {
-  console.log('navigator.userAgentData nije podržan.');
 }
